e07a012d51db98c860ced57b53f8163098e9622c
Yan Kaiky <yankaikys@gmail.com>
2023-12-24 10:50:48 -0300

    feat(App): add Data

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
+import Monsters from './services/monster.energy/index.json';
+import RedBulls from './services/redbull/index.json';
 import './App.css';
 
+interface Drink {
+  name: string;
+  brand: string;
+  description: string;
+  category: string;
+  image: string;
+}
+
 const App = () => {
+  const data: Drink[] = [...Monsters, ...RedBulls];
+
   return (
-    <>
+    <div id='content'>
       <header>
         <div className="logo">YanKaiky</div>
 
@@ -22,94 +34,20 @@ const App = () => {
 
       <div className="slider">
         <div className="list">
-          <div className="item active">
-            <img src="./images/monster-energy/JUICE-MANGO-LOCO.png" alt="JUICE MANGO LOCO" />
+          {data.map((drk) => {
+            return (
+              <div className="item active">
+                <img src={drk.image} alt={drk.name} />
 
-            <div className="content">
-              <p>Design</p>
-              <h2>JUICE MANGO LOCO</h2>
+                <div className="content">
+                  <p>Design</p>
+                  <h2>{drk.name}</h2>
 
-              <p>Mix de sucos perfeito para atrair as almas mais teimosas. Inspirado pelas tradições mexicanas e com um mix de frutas com manga. Além de sabor também entrega energia!</p>
-            </div>
-          </div>
-
-          <div className="item">
-            <img src="./images/monster-energy/ABSOLUTELY-ZERO.png" alt="ABSOLUTELY ZERO" />
-
-            <div className="content">
-              <p>Design</p>
-              <h2>ABSOLUTELY-ZERO</h2>
-
-              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ullam, tempora? Expedita quam quos nisi
-                consequatur, fuga explicabo dolore fugiat! Ex nihil mollitia consequatur qui ullam odio dolorum
-                quod modi accusantium?</p>
-            </div>
-          </div>
-
-          <div className="item">
-            <img src="./images/monster-energy/DRAGON-ICE-TEA-LIMÃO.png" alt="DRAGON ICE TEA LIMÃO" />
-
-            <div className="content">
-              <p>Design</p>
-              <h2>DRAGON ICE TEA LIMÃO</h2>
-
-              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ullam, tempora? Expedita quam quos nisi
-                consequatur, fuga explicabo dolore fugiat! Ex nihil mollitia consequatur qui ullam odio dolorum
-                quod modi accusantium?</p>
-            </div>
-          </div>
-
-          <div className="item">
-            <img src="./images/monster-energy/DRAGON-ICE-TEA-PÊSSEGO.png" alt="DRAGON ICE TEA PÊSSEGO" />
-
-            <div className="content">
-              <p>Design</p>
-              <h2>DRAGON ICE TEA PÊSSEGO</h2>
-
-              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ullam, tempora? Expedita quam quos nisi
-                consequatur, fuga explicabo dolore fugiat! Ex nihil mollitia consequatur qui ullam odio dolorum
-                quod modi accusantium?</p>
-            </div>
-          </div>
-
-          <div className="item">
-            <img src="./images/monster-energy/JUICE-KHAOS.png" alt="JUICE KHAOS" />
-
-            <div className="content">
-              <p>Design</p>
-              <h2>JUICE KHAOS</h2>
-
-              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ullam, tempora? Expedita quam quos nisi
-                consequatur, fuga explicabo dolore fugiat! Ex nihil mollitia consequatur qui ullam odio dolorum
-                quod modi accusantium?</p>
-            </div>
-          </div>
-
-          <div className="item">
-            <img src="./images/monster-energy/JUICE-KHAOTIC.png" alt="JUICE KHAOTIC" />
-
-            <div className="content">
-              <p>Design</p>
-              <h2>JUICE KHAOTIC</h2>
-
-              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ullam, tempora? Expedita quam quos nisi
-                consequatur, fuga explicabo dolore fugiat! Ex nihil mollitia consequatur qui ullam odio dolorum
-                quod modi accusantium?</p>
-            </div>
-          </div>
-
-          <div className="item">
-            <img src="./images/monster-energy/JUICE-PACIFIC-PUNCH.png" alt="JUICE PACIFIC PUNCH" />
-
-            <div className="content">
-              <p>Design</p>
-              <h2>JUICE PACIFIC PUNCH</h2>
-
-              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ullam, tempora? Expedita quam quos nisi
-                consequatur, fuga explicabo dolore fugiat! Ex nihil mollitia consequatur qui ullam odio dolorum
-                quod modi accusantium?</p>
-            </div>
-          </div>
+                  <p>{drk.description}</p>
+                </div>
+              </div>
+            )
+          })}
         </div>
 
         <div className="arrows">
@@ -118,43 +56,17 @@ const App = () => {
         </div>
 
         <div className="thumbnails">
-          <div className="item active">
-            <img src="./images/monster-energy/JUICE-MANGO-LOCO.png" alt="JUICE MANGO LOCO" />
-            <div className="content">JUICE MANGO LOCO</div>
-          </div>
-
-          <div className="item">
-            <img src="./images/monster-energy/ABSOLUTELY-ZERO.png" alt="ABSOLUTELY ZERO" />
-            <div className="content">ABSOLUTELY ZERO</div>
-          </div>
-
-          <div className="item">
-            <img src="./images/monster-energy/DRAGON-ICE-TEA-LIMÃO.png" alt="DRAGON ICE TEA LIMÃO" />
-            <div className="content">DRAGON ICE TEA LIMÃO</div>
-          </div>
-
-          <div className="item">
-            <img src="./images/monster-energy/DRAGON-ICE-TEA-PÊSSEGO.png" alt="DRAGON ICE TEA PÊSSEGO" />
-            <div className="content">DRAGON ICE TEA PÊSSEGO</div>
-          </div>
-
-          <div className="item">
-            <img src="./images/monster-energy/JUICE-KHAOS.png" alt="JUICE KHAOS" />
-            <div className="content">JUICE KHAOS</div>
-          </div>
-
-          <div className="item">
-            <img src="./images/monster-energy/JUICE-KHAOTIC.png" alt="JUICE KHAOS" />
-            <div className="content">JUICE KHAOTIC</div>
-          </div>
-
-          <div className="item">
-            <img src="./images/monster-energy/JUICE-PACIFIC-PUNCH.png" alt="JUICE PACIFIC PUNCH" />
-            <div className="content">JUICE PACIFIC PUNCH</div>
-          </div>
+          {data.map((drk) => {
+            return (
+              <div className="item active">
+                <img src={drk.image} alt={drk.name} />
+                <div className="content">{drk.name}</div>
+              </div>
+            )
+          })}
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
